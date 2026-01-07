5dee9c42723995f3735e8ae8ac401fc082d508f9
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-07 21:18:24 +0700

    feat(category): using fluent api configuration model category

--- ShopDBProduct/Services/CategoryService.cs
+++ ShopDBProduct/Services/CategoryService.cs
@@ -1,6 +1,25 @@
-﻿namespace ShopDBProduct.Services
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopDBProduct.Entities;
+
+namespace ShopDBProduct.Services
 {
-    public class CategoryService
+    public class CategoryService : IEntityTypeConfiguration<Category>
     {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("categories").HasKey(p => p.Id);
+
+            builder.Property(c => c.Name).IsRequired().HasMaxLength(255);
+            builder.Property(c => c.Description).IsRequired().HasMaxLength(255);
+            builder.Property(c => c.Status).IsRequired(false).HasDefaultValue(false);
+            builder
+                .HasMany(c => c.Products)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
     }
 }
