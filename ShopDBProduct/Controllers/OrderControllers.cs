9732d1efb84913444efc08f09965d883109e1d98
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-10 00:24:43 +0700

    feat(order): Done controller

---
+++ ShopDBProduct/Controllers/OrderControllers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopDBProduct.Controllers.Base;
+using ShopDBProduct.DTOs.Orders;
+using ShopDBProduct.Services.Interfaces;
+
+namespace ShopDBProduct.Controllers
+{
+    [Route("api/v1/orders")]
+    public class OrderControllers : BaseApiController
+    {
+        private readonly IOrderService _orderService;
+        public OrderControllers(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateOrderDto dto)
+        {
+            var orderId = await _orderService.CreateOrderAsync(dto);
+            return Ok(orderId);
+        }
+    }
+}
