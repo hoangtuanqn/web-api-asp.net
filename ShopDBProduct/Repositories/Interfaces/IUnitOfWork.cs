bc68181fd0869e2a59e14ade5c12520beff790fc
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-12 22:53:22 +0700

    chore: inherit IDisposable in interface IUnitOfWork

--- ShopDBProduct/Repositories/Interfaces/IUnitOfWork.cs
+++ ShopDBProduct/Repositories/Interfaces/IUnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿namespace ShopDBProduct.Repositories.Interfaces
 {
-    public interface IUnitOfWork
+    // IDisposable inherit nó sẽ tự động dọn dẹp sau khi sử dụng xong
+    public interface IUnitOfWork: IDisposable
     {
         public Task<int> SaveChangeAsync();
     }
