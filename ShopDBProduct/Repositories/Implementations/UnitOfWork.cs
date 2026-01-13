f759ea3559a49a7d521862c8f24987042c6b3b38
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-14 03:31:34 +0700

    Refactor UnitOfWork: lazy repo init & transaction support

--- ShopDBProduct/Repositories/Implementations/UnitOfWork.cs
+++ ShopDBProduct/Repositories/Implementations/UnitOfWork.cs
@@ -1,5 +1,7 @@
-﻿using ShopDBProduct.Data;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using ShopDBProduct.Data;
 using ShopDBProduct.Repositories.Interfaces;
+using ShopDBProduct.Services.Implementations;
 
 namespace ShopDBProduct.Repositories.Implementations
 {
@@ -7,29 +9,64 @@ namespace ShopDBProduct.Repositories.Implementations
     public class UnitOfWork : IUnitOfWork
     {
         private readonly AppDbContext _context;
-        public IProductRepository Products { get; }
-        public ICategoryRepository Categories { get; }
-        public IOrderRepository Orders { get; }
+        private IDbContextTransaction? _transaction;
+
+        private ICategoryRepository? _categoryRepository;
+        private IProductRepository? _productRepository;
+        private IOrderRepository? _orderRepository;
 
 
-        public UnitOfWork(
-            AppDbContext context,
-            IProductRepository productRepository,
-            ICategoryRepository categoryRepository,
-            IOrderRepository orderRepository
-         )
+        public ICategoryRepository Categories => _categoryRepository ??= new CategoryRepository(_context);
+        public IProductRepository Products => _productRepository ??= new ProductRepository(_context);
+        public IOrderRepository Orders => _orderRepository ??= new OrderRepository(_context);
+
+        public UnitOfWork(AppDbContext context)
         {
             _context = context;
-            Products = productRepository;
-            Categories = categoryRepository;
-            Orders = orderRepository;
         }
 
 
+        public async Task BeginTransaction()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
 
+        public async Task RollBackAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync();
+            }
+        }
+
+        // Lưu vô ram
         public async Task<int> SaveChangeAsync()
         {
             return await _context.SaveChangesAsync();
+        }
+
+        // Chốt hạ xuống DB
+        public async Task CommitAsync()
+        {
+            try
+            {
+                if (_transaction != null)
+                {
+                    await _transaction.CommitAsync();
+                }
+            }
+            catch
+            {
+                await RollBackAsync();
+            }
+            finally
+            {
+                if (_transaction != null)
+                {
+                    await _transaction.DisposeAsync();
+                    _transaction = null;
+                }
+            }
         }
         public void Dispose()
         {
