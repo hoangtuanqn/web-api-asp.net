b72cb7bf35dcaf02ff4f0b6941e69c3f0171e54d
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-15 17:08:58 +0700

    feat: validator login dto

---
+++ ShopDBProduct/Validators/Auth/LoginValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ShopDBProduct.DTOs.Users;
+
+namespace ShopDBProduct.Validators.Auth
+{
+    public class LoginValidator: AbstractValidator<LoginDto>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email của bạn không hợp lệ!");
+            RuleFor(x => x.Password).MinimumLength(6).WithMessage("Mật khẩu phải có tối thiểu 6 kí tự");
+        }
+    }
+}
