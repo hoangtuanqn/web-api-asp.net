13b190b284e8c5a1a0940f0d8394984ef9b2cdec
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-15 17:05:44 +0700

    chore: logic basic login controller

---
+++ ShopDBProduct/Controllers/AuthControllers.cs
@@ -0,0 +1,34 @@
+﻿using BCrypt.Net;
+using Microsoft.AspNetCore.Mvc;
+using ShopDBProduct.Controllers.Base;
+using ShopDBProduct.DTOs.Users;
+using ShopDBProduct.Entities;
+using ShopDBProduct.Services.Interfaces;
+
+namespace ShopDBProduct.Controllers
+{
+    public class AuthControllers : BaseApiController
+    {
+
+        private readonly IAuthService _authService;
+
+        public AuthControllers(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto dto)
+        {
+            // Giả sử đã lấy user từ DB
+            var user = new User
+            {
+                Email = dto.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword("123456"),
+            };
+
+            var result = _authService.Login(user, dto.Password);
+            return Ok(result);
+        }
+    }
+}
---
+++ ShopDBProduct/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using ShopDBProduct.DTOs.Users;
+using ShopDBProduct.Entities;
+
+namespace ShopDBProduct.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        public AuthResponseDto Login(User user, string password);
+
+    }
+}
