9d97dfc89f8ffa9548fb9b040e749c68919a2ca1
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-15 16:58:50 +0700

    feat: logic generate jwt access token

---
+++ ShopDBProduct/Migrations/20260115092854_add-table-users.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopDBProduct.Migrations
+{
+    /// <inheritdoc />
+    public partial class addtableusers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_users", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ ShopDBProduct/Utils/JwtTokenGenerator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using ShopDBProduct.DTOs.Users;
+using ShopDBProduct.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ShopDBProduct.Utils
+{
+    public class JwtTokenGenerator
+    {
+        private readonly IConfiguration _config;
+        public JwtTokenGenerator(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public AuthResponseDto GenerateAccessToken(User user)
+        {
+            var jwt = _config.GetSection("JWT");
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expires = DateTime.UtcNow.AddMinutes(double.Parse(jwt["ExpireMinutes"]!));
+            var token = new JwtSecurityToken(
+                issuer: jwt["Issuer"],
+                audience: jwt["Audience"],
+                claims: claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new AuthResponseDto
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(token),
+            };
+        }
+    }
+}
